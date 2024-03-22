e5998decef41fecd8def9538b37c234c14baf790
김주영 <gimjuyeong0620@gmail.com>
2024-03-22 22:37:28 +0900

    feat : first commit

---
+++ src/main/java/poten012/sonmong/Poten403/Poten403Application.java
@@ -0,0 +1,13 @@
+package poten012.sonmong.Poten403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poten403Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poten403Application.class, args);
+	}
+
+}
---
+++ src/test/java/poten012/sonmong/Poten403/Poten403ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Poten403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poten403ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
